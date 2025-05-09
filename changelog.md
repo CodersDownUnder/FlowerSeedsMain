3090077629ae2846b25ccc2bc72e81374f91c6d1
Thomas <hedgehog.thomas@gmail.com>
2025-05-09 16:44:22 +1000

    Fixed Item Localization, Added auto publishing

---
+++ changelog.md
@@ -0,0 +1,46 @@
+## V3.1.0 Minecraft 1.21.1
+
+---
+
+Fixed Item Names
+
+## V3.0.0
+ 
+---
+
+Port to 1.21.4
+Port to 1.21.1
+
+## V2.0.0
+
+---
+
+- Various Improvments
+
+
+## V1.2.0
+
+---
+
+- Updated to 1.20.4
+- Now using Compostables Data Map
+
+
+## V1.1.2
+
+---
+
+- Made Stage 0 crop models more visible by moving them up 0.04 of a block
+- Fixed crops giving wheat seeds when middle-clicked in creative
+
+## V1.1.1
+
+---
+
+- Fixed crash on server start
+
+## V1.1.0
+
+---
+
+- Added log filter to remove conditional recipe spam.
--- src/datagen/java/net/codersdownunder/flowerseeds/datagen/client/lang/EN_US.java
+++ src/datagen/java/net/codersdownunder/flowerseeds/datagen/client/lang/EN_US.java
@@ -3,6 +3,7 @@ package net.codersdownunder.flowerseeds.datagen.client.lang;
 import net.codersdownunder.flowerseeds2.FlowerSeeds2;
 import net.codersdownunder.flowerseeds2.init.CropRegistries;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.data.LanguageProvider;
 
 public class EN_US extends LanguageProvider {
@@ -21,19 +22,19 @@ public class EN_US extends LanguageProvider {
     @Override
     protected void addTranslations() {
 
-        add(CropRegistries.DANDELION_CROP.get(), "Dandelion Seeds");
-        add(CropRegistries.WITHER_ROSE_CROP.get(), "Wither Rose Seeds");
-        add(CropRegistries.OXEYE_DAISY_CROP.get(), "Oxeye Daisy");
-        add(CropRegistries.POPPY_CROP.get(), "Poppy");
-        add(CropRegistries.PINK_TULIP_CROP.get(), "Pink Tulip");
-        add(CropRegistries.RED_TULIP_CROP.get(), "Red Tulip");
-        add(CropRegistries.WHITE_TULIP_CROP.get(), "White Tulip");
-        add(CropRegistries.ORANGE_TULIP_CROP.get(), "Orange Tulip");
-        add(CropRegistries.LILY_OF_THE_VALLEY_CROP.get(), "Lily of the Valley");
-        add(CropRegistries.BLUE_ORCHID_CROP.get(), "Blue Orchid");
-        add(CropRegistries.CORNFLOWER_CROP.get(), "Cornflower");
-        add(CropRegistries.AZURE_BLUET_CROP.get(), "Azure Bluet");
-        add(CropRegistries.ALLIUM_CROP.get(), "Allium");
+        addTranslation(CropRegistries.DANDELION_CROP.get(), "Dandelion");
+        addTranslation(CropRegistries.WITHER_ROSE_CROP.get(), "Wither Rose");
+        addTranslation(CropRegistries.OXEYE_DAISY_CROP.get(), "Oxeye Daisy");
+        addTranslation(CropRegistries.POPPY_CROP.get(), "Poppy");
+        addTranslation(CropRegistries.PINK_TULIP_CROP.get(), "Pink Tulip");
+        addTranslation(CropRegistries.RED_TULIP_CROP.get(), "Red Tulip");
+        addTranslation(CropRegistries.WHITE_TULIP_CROP.get(), "White Tulip");
+        addTranslation(CropRegistries.ORANGE_TULIP_CROP.get(), "Orange Tulip");
+        addTranslation(CropRegistries.LILY_OF_THE_VALLEY_CROP.get(), "Lily of the Valley");
+        addTranslation(CropRegistries.BLUE_ORCHID_CROP.get(), "Blue Orchid");
+        addTranslation(CropRegistries.CORNFLOWER_CROP.get(), "Cornflower");
+        addTranslation(CropRegistries.AZURE_BLUET_CROP.get(), "Azure Bluet");
+        addTranslation(CropRegistries.ALLIUM_CROP.get(), "Allium");
         //add(CropRegistries.EYEBLOSSOM_CROP.get(), "Eyeblossom Seeds");
 
         add("flowerseeds2.packmeta.description", "Flower Seeds 2 Resource pack");
@@ -49,4 +50,9 @@ public class EN_US extends LanguageProvider {
         add("flowerseeds2.configuration.witherrosetradelevelcleric", "Wither Rose Cleric Trade Level");
     }
 
+
+    public void addTranslation(Block block, String blockName) {
+        add(block, blockName);
+        add(block.asItem().getDefaultInstance(), blockName + " Seeds");
+    }
 }
